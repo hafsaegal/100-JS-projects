dc0d102865fd3608f15d84eab94fa37a58a5a403
Hafsa Egal <hafsaegal915@gmail.com>
2025-04-03 18:13:30 +0300

    pomodoro timer

---
+++ script.js
@@ -0,0 +1,15 @@
+const start= document.getElementById("start");
+const stop= document.getElementById("stop");
+const reset= document.getElementById("reset");
+const timer= document.getElementById("timer");
+
+let timeLeft=1500;
+let interval;
+
+const updateTimer= () => {
+    const minutes= Math.floor(timeLeft/60);
+    const seconds= timeLeft % 60;
+
+    timer.innerHTML = `${minutes}:${seconds}`;
+    
+}
